b8527eeab263538b5f0b61edb9b7741700b8e369
samueledwardwinoto <6182101040@student.unpar.ac.id>
2023-12-20 15:10:07 +0700

    Edit Schedule Page

--- public/js/trial-result-overview-script.js
+++ public/js/trial-result-overview-script.js
@@ -1,4 +1,4 @@
-function showPopup() {
+/*function showPopup() {
 	document.getElementById("popup").style.display = "block";
 	// Di sini kamu bisa menambahkan logika untuk mengambil catatan sidang dan nilai sidang dari database atau sumber lainnya
 	// Contoh:
@@ -10,4 +10,4 @@ function showPopup() {
 
 function closePopup() {
 	document.getElementById("popup").style.display = "none";
-}
+}*/
