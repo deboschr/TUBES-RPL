30cc07de9d182eaf8aa9840165829d8a3abe00ed
cornel <135084170+robot-afk@users.noreply.github.com>
2024-01-02 08:55:44 +0700

    menambah halaman user detail dan user overview management

--- src/controllers/home-controller.js
+++
@@ -1,9 +0,0 @@
-export const home_page = (req, res) => {
-	res.render("home", {
-		title: "Home",
-		layout: "layouts/main",
-		style: "home-style.css",
-		script: "home-script.js",
-		data_user: "ini data user",
-	});
-};
--- src/controllers/user-management-controller.js
+++
@@ -1,9 +0,0 @@
-export const user_management_page = (req, res) => {
-	res.render("user-management", {
-		title: "Manajemen Pengguna",
-		layout: "layouts/main",
-		style: "user-management-style.css",
-		script: "user-management-script.js",
-		data_user: "ini data user",
-	});
-};
